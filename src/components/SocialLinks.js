c26a1f35c02d1c5477af0656945243ad176148b0
Roman Schaller <roman.schaller@gmail.com>
2019-12-05 21:47:41 +0100

    fix url for social links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   FacebookShareButton,
   LinkedinShareButton,
@@ -16,49 +16,54 @@ import {
 } from 'react-share'
 import urljoin from 'url-join'
 import config from '../../data/SiteConfig'
-import styles from  './SocialLinks.module.scss'
+import styles from './SocialLinks.module.scss'
 
 const SocialLinks = ({ postNode, postPath, mobile }) => {
-    const post = postNode.frontmatter;
-    const url = urljoin(config.siteUrl, config.pathPrefix, postPath);
-    const iconSize = mobile ? 32 : 32;
-    const filter = count => (count > 0 ? count : "");
-    const renderShareCount = count => (
-      <div className={styles.shareCount}>{filter(count)}</div>
-    );
+  const [url, setUrl] = useState(urljoin(config.siteUrl, config.pathPrefix, postPath));
+  const post = postNode.frontmatter;
+  const iconSize = mobile ? 32 : 32;
+  const filter = count => (count > 0 ? count : "");
+  const renderShareCount = count => (
+    <div className={styles.shareCount}>{filter(count)}</div>
+  );
 
-    return (
-      <div className={styles.socialLinks}>
-        <RedditShareButton url={url} title={post.title}>
-          <RedditIcon round size={iconSize} />
-          <RedditShareCount url={url}>
-            {count => renderShareCount(count)}
-          </RedditShareCount>
-        </RedditShareButton>
-        <TwitterShareButton url={url} title={post.title}>
-          <TwitterIcon round size={iconSize} />
-        </TwitterShareButton>
-        <FacebookShareButton url={url} quote={postNode.excerpt}>
-          <FacebookIcon round size={iconSize} />
-          <FacebookShareCount url={url}>
-            {count => renderShareCount(count)}
-          </FacebookShareCount>
-        </FacebookShareButton>
-        <LinkedinShareButton
-          url={url}
-          title={post.title}
-          description={postNode.excerpt}
-        >
-          <LinkedinIcon round size={iconSize} />
-          <LinkedinShareCount url={url}>
-            {count => renderShareCount(count)}
-          </LinkedinShareCount>
-        </LinkedinShareButton>
-        <TelegramShareButton url={url}>
-          <TelegramIcon round size={iconSize} />
-        </TelegramShareButton>
-      </div>
-    )
-  }
+  // set the url again after the componend was mounted.
+  useEffect(() => {
+    setUrl(window.location.href);
+  })
+
+  return (
+    <div className={styles.socialLinks}>
+      <RedditShareButton url={url} title={post.title}>
+        <RedditIcon round size={iconSize} />
+        <RedditShareCount url={url}>
+          {count => renderShareCount(count)}
+        </RedditShareCount>
+      </RedditShareButton>
+      <TwitterShareButton url={url} title={post.title}>
+        <TwitterIcon round size={iconSize} />
+      </TwitterShareButton>
+      <FacebookShareButton url={url} quote={postNode.excerpt}>
+        <FacebookIcon round size={iconSize} />
+        <FacebookShareCount url={url}>
+          {count => renderShareCount(count)}
+        </FacebookShareCount>
+      </FacebookShareButton>
+      <LinkedinShareButton
+        url={url}
+        title={post.title}
+        description={postNode.excerpt}
+      >
+        <LinkedinIcon round size={iconSize} />
+        <LinkedinShareCount url={url}>
+          {count => renderShareCount(count)}
+        </LinkedinShareCount>
+      </LinkedinShareButton>
+      <TelegramShareButton url={url}>
+        <TelegramIcon round size={iconSize} />
+      </TelegramShareButton>
+    </div>
+  )
+}
 
 export default SocialLinks
