669a4f5de0263b9249fdac1c94fae14fb77fc956
Roman Schaller <roman.schaller@gmail.com>
2019-12-05 22:22:11 +0100

    Add contact information

---
+++ src/pages/contact.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../layout'
+import config from '../../data/SiteConfig'
+import { Link } from 'gatsby'
+
+const ContactPage = () => {
+  const emailEncoded = 'cm9tYW4uc2NoYWxsZXJAZ21haWwuY29t'; // my email address encoded with function btoa() => spam secured
+  const [email, setEmail] = useState(emailEncoded);
+  useEffect(() => {
+    setEmail(atob(emailEncoded));
+  });
+
+  return (
+    <Layout>
+      <main>
+        <Helmet title={`Contact | ${config.siteTitle}`} />
+        <h1>Contact me</h1>
+        <p>
+          You can reach me at <Link to="https://www.twitter.com/romixch">Twitter</Link>. Or you can simply send me an email to <a href={`mailto:${email}`}>{email}</a>.
+        </p>
+      </main>
+    </Layout>
+  )
+}
+export default ContactPage;
